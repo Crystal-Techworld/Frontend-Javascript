9964540b3bf2da3a8730b76ff8b293f037089e92
Crystal <Johnowolabi41@yahoo.com>
2020-03-25 12:09:15 +0100

    new javascript file

---
+++ script.js
@@ -0,0 +1,21 @@
+// my name in variable
+var firstName = "Opeyemi";
+var lastName = " Owolabi";
+
+console.log (firstName + lastName);
+
+// my courses in an array
+var myCourses = [" HTML", " CSS", " JAVASCRIPT"];
+
+console.log ( myCourses)
+
+
+// Number of courses (3) is odd umber, therefore to display odd number from 1 to 200
+{
+for 
+(let  c = 0; c < 200; c ++)
+
+if (c % 2 === 0) 
+
+console.log (c += 1);
+}
